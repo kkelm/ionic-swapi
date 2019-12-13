4910ae0b7f8c2b73689acb4fd39fc80296aa8b02
Kevin Kelm <kkelm@outlook.com>
2019-12-12 19:21:33 -0600

    Got all the planets

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { merge } from 'rxjs';
 
 
 @Injectable({
@@ -10,9 +11,23 @@ export class SwapiService {
 	constructor(private httpClient: HttpClient) { }
 
 	getPlanets() {
-		const observablePlanets = this.httpClient.get("https://swapi.co/api/planets");
+		const observablePlanetsOne = this.httpClient.get("https://swapi.co/api/planets");
+		const observablePlanetsTwo = this.httpClient.get("https://swapi.co/api/planets?page=2");
+		const observablePlanetsThree = this.httpClient.get("https://swapi.co/api/planets?page=3");
+		const observablePlanetsFour = this.httpClient.get("https://swapi.co/api/planets?page=4");
+		const observablePlanetsFive = this.httpClient.get("https://swapi.co/api/planets?page=5");
+		const observablePlanetsSix = this.httpClient.get("https://swapi.co/api/planets?page=6");
+		const observablePlanetsSeven = this.httpClient.get("https://swapi.co/api/planets?page=7");
 
-		return observablePlanets;
+		return merge(
+			observablePlanetsOne
+			, observablePlanetsTwo
+			, observablePlanetsThree
+			, observablePlanetsFour
+			, observablePlanetsFive
+			, observablePlanetsSix
+			, observablePlanetsSeven
+		);
 	}
 
 }
